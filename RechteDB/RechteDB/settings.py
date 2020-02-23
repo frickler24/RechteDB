4773ad11ac172a4db47bf517c897a8397a58eab4
Lutz Eichler <github@frickler24.de>
2020-02-23 16:27:31 +0100

    fix: change tabs in settings.py to spaces

--- RechteDB/RechteDB/settings.py
+++ RechteDB/RechteDB/settings.py
@@ -29,62 +29,62 @@ INTERNAL_IPS = config('INTERNAL_IPS')
 import dj_database_url
 
 DATABASES = {
-	'default': dj_database_url.config (
-		default = config('DATABASE_URL'),
-        conn_max_age = 600,
-	)
+    'default': dj_database_url.config (
+        default=config('DATABASE_URL'),
+        conn_max_age=600,
+    )
 }
 
 # Application definition
 
 INSTALLED_APPS = [
-	'django.contrib.admin',
-	'django.contrib.auth',
-	'django.contrib.contenttypes',
-	'django.contrib.sessions',
-	'django.contrib.messages',
-	'django.contrib.staticfiles',
-	'bootstrap4',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'bootstrap4',
 
-	'rapp',
-	'django_filters',
-	'debug_toolbar',
+    'rapp',
+    'django_filters',
+    'debug_toolbar',
 
-	'widget_tweaks',
-	'django_tables2',
-	'import_export',
-	'mdeditor',
+    'widget_tweaks',
+    'django_tables2',
+    'import_export',
+    'mdeditor',
 ]
 
 MIDDLEWARE = [
-	'debug_toolbar.middleware.DebugToolbarMiddleware',
-	'django.middleware.security.SecurityMiddleware',
-	'django.contrib.sessions.middleware.SessionMiddleware',
-	'django.middleware.common.CommonMiddleware',
-	'django.middleware.csrf.CsrfViewMiddleware',
-	'django.contrib.auth.middleware.AuthenticationMiddleware',
-	'django.contrib.messages.middleware.MessageMiddleware',
-	'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'RechteDB.urls'
 
 TEMPLATES = [
-	{
-		'BACKEND': 'django.template.backends.django.DjangoTemplates',
-		'DIRS': [
-			os.path.join(BASE_DIR, 'templates'),
-		],
-		'APP_DIRS': True,
-		'OPTIONS': {
-			'context_processors': [
-				'django.template.context_processors.debug',
-				'django.template.context_processors.request',
-				'django.contrib.auth.context_processors.auth',
-				'django.contrib.messages.context_processors.messages',
-			],
-		},
-	},
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
 ]
 
 #WSGI_APPLICATION = 'RechteDB.wsgi.application'
@@ -96,18 +96,18 @@ TEMPLATES = [
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 AUTH_PASSWORD_VALIDATORS = [
-	{
-		'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-	},
-	{
-		'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-	},
-	{
-		'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-	},
-	{
-		'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-	},
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
 ]
 
 
@@ -142,32 +142,32 @@ IMPORT_EXPORT_EXPORT_PERMISSION_CODE = 'add'
 
 # MDEditor-Settings
 MDEDITOR_CONFIGS = {
-	'default': {
-		'width': '90% ',  # Custom edit box width
-		'heigth': 500,  # Custom edit box height
-		'toolbar': ["undo", "redo", "|",
-					"bold", "del", "italic", "quote", "ucwords", "uppercase", "lowercase", "|",
-					"h1", "h2", "h3", "h4", "h5", "h6", "|",
-					"list-ul", "list-ol", "hr", "|",
-					"link", "reference-link", "image", "code", "preformatted-text", "code-block", "table", "datetime"
-																										   "emoji",
-					"html-entities", "pagebreak", "goto-line", "|",
-					"help", "info",
-					"||", "preview", "watch", "fullscreen"],  # custom edit box toolbar
-		'upload_image_formats': ["jpg", "jpeg", "gif", "png", "bmp", "webp"],  # image upload format type
-		'image_folder': 'editor',  # image save the folder name
-		'theme': 'default',  # edit box theme, dark / default
-		'preview_theme': 'default',  # Preview area theme, dark / default
-		'editor_theme': 'default',  # edit area theme, pastel-on-dark / default
-		'toolbar_autofixed': True,  # Whether the toolbar capitals
-		'search_replace': True,  # Whether to open the search for replacement
-		'emoji': True,  # whether to open the expression function
-		'tex': True,  # whether to open the tex chart function
-		'flow_chart': True,  # whether to open the flow chart function
-		'sequence': True,  # Whether to open the sequence diagram function
-		'watch': True,  # Live preview
-		'lineWrapping': False,  # lineWrapping
-		'lineNumbers': True,  # lineNumbers
-		'language': 'de'
-	}
+    'default': {
+        'width': '90% ',  # Custom edit box width
+        'heigth': 500,  # Custom edit box height
+        'toolbar': ["undo", "redo", "|",
+                    "bold", "del", "italic", "quote", "ucwords", "uppercase", "lowercase", "|",
+                    "h1", "h2", "h3", "h4", "h5", "h6", "|",
+                    "list-ul", "list-ol", "hr", "|",
+                    "link", "reference-link", "image", "code", "preformatted-text", "code-block", "table", "datetime"
+                                                                                                           "emoji",
+                    "html-entities", "pagebreak", "goto-line", "|",
+                    "help", "info",
+                    "||", "preview", "watch", "fullscreen"],  # custom edit box toolbar
+        'upload_image_formats': ["jpg", "jpeg", "gif", "png", "bmp", "webp"],  # image upload format type
+        'image_folder': 'editor',  # image save the folder name
+        'theme': 'default',  # edit box theme, dark / default
+        'preview_theme': 'default',  # Preview area theme, dark / default
+        'editor_theme': 'default',  # edit area theme, pastel-on-dark / default
+        'toolbar_autofixed': True,  # Whether the toolbar capitals
+        'search_replace': True,  # Whether to open the search for replacement
+        'emoji': True,  # whether to open the expression function
+        'tex': True,  # whether to open the tex chart function
+        'flow_chart': True,  # whether to open the flow chart function
+        'sequence': True,  # Whether to open the sequence diagram function
+        'watch': True,  # Live preview
+        'lineWrapping': False,  # lineWrapping
+        'lineNumbers': True,  # lineNumbers
+        'language': 'de'
+    }
 }
