f8e383136a17bfa74c4fc1cab889e377bd346a52
Lutz Eichler <github@frickler24.de>
2020-05-16 19:28:03 +0200

    forms.py

--- RechteDB/rapp/forms.py
+++ RechteDB/rapp/forms.py
@@ -1,13 +1,16 @@
+from urllib.parse import unquote
+
 from django import forms
-from urllib.parse import quote, unquote
 
 from .models import TblUserhatrolle
 from .models import hole_organisationen
-from .models import Manuelle_Berechtigung
 
 
-# Das hätte man auch einfacher haben können, indem die relevanten Infos in views.py eingetragen worden wären
 class ShowUhRForm(forms.ModelForm):
+    """
+    Das hätte man auch einfacher haben können, indem die relevanten Infos in views.py eingetragen worden wären
+    """
+
     class Meta:
         model = TblUserhatrolle
         fields = ['userid', 'rollenname', 'schwerpunkt_vertretung', 'bemerkung', ]
@@ -16,6 +19,10 @@ class ShowUhRForm(forms.ModelForm):
 # Hier ist das anders, weil eine Methode zur Klasse hinzugekommen ist:
 # Initialisiere das Input Formular für neue Rolleneinträge mit der UserID, dem Modell und der Zuständigkeitsstufe
 class CreateUhRForm(forms.ModelForm):
+    """
+    Das hätte man auch einfacher haben können, indem die relevanten Infos in views.py eingetragen worden wären
+    """
+
     class Meta:
         model = TblUserhatrolle
         fields = ['userid', 'rollenname', 'schwerpunkt_vertretung', 'bemerkung', ]
@@ -40,9 +47,11 @@ class CreateUhRForm(forms.ModelForm):
         self.initial['schwerpunkt_vertretung'] = self.schwerpunkt_vertretung
 
 
-# Auch hier ist das Thema das Initialisieren des Organisations-Choicefields
 class ImportForm(forms.Form):
-    # Die ersten Parameter, die für einen CSV-Import abgefragt werden müssen
+    """
+    Die ersten Parameter, die für einen CSV-Import abgefragt werden müssen.
+    Hier ist das Thema das Initialisieren des Organisations-Choicefields.
+    """
     organisation = forms.ChoiceField(label='Organisation')
     datei = forms.FileField(label='Dateiname')
     update_gruppe = forms.BooleanField(required=False, initial=False, label='Gruppenzugehörigkeit aktualisieren')
@@ -52,16 +61,20 @@ class ImportForm(forms.Form):
         self.fields['organisation'].choices = hole_organisationen()
 
 
-# Das hier behandelte boolean Field ist nicht Inhalt des Models, sondern ändert lediglich den Workflow
 class ImportFormSchritt3(forms.Form):
-    # Der Abschluss des zweiten Schritts besteht ebenfalls nur aus einer Bestätigung,
-    # deshalb sind auch hier keine Datenfelder angegeben
-    # (Eventuell kann hier noch ein Flag angegeben werden, ob Doppeleinträge gesucht wertden sollen)
+    """
+    Das hier behandelte boolean Field ist nicht Inhalt des Models, sondern ändert lediglich den Workflow.
+    Der Abschluss des zweiten Schritts besteht ebenfalls nur aus einer Bestätigung,
+    deshalb sind auch hier keine Datenfelder angegeben
+    (eventuell kann hier noch ein Flag angegeben werden, ob Doppeleinträge gesucht wertden sollen).
+    """
     doppelte_suchen = forms.BooleanField(label='Suche nach doppelten Einträgen (optional)', required=False)
 
 
-# Formular für das Umbenennen von Rollen
 class FormUmbenennen(forms.Form):
+    """
+    Formular für das Umbenennen von Rollen
+    """
     alter_name = forms.CharField(max_length=50, label='Bestehender Rollenname',
                                  error_messages={'required': 'Bitte geben Sie den bestehenden Rollennamen an',
                                                  'invalid': 'Bestehender Rollennamen wird benötigt'})
